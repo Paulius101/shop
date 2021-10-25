4a3857477dddbefd6dec5f97010094a6d0885c53
Gytis Žaltauskas <gytis.zaltauskas@gmail.com>
2021-10-25 20:26:44 +0300

    Parduotuvė

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemComponent } from './components/item/item.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent,
+    ItemComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
